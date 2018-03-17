f9adfb5a36b2d24e34131ea120b433f6a5fde0d6
Shirish111 <shirishtogarla533@gmail.com>
2018-03-17 15:23:29 +0530

    Used Support Vector Machines

--- sentence_structure.py
+++ sentence_structure.py
@@ -1,12 +1,17 @@
 import nltk
 import pandas as pd
-from sklearn import linear_model
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.cross_validation import cross_val_score, train_test_split
+from sklearn.metrics import confusion_matrix
+from sklearn.svm import SVC
 from sklearn.utils import shuffle
-from unidecode import unidecode
 
 
 def tokenize(fname):
+    """
+    @param fname = filename
+    Returns = list of sentences, where each sentence is a list of POS
+              tagged words
+    """
     f = open(fname, "r")
     taggedSents = []
     s = f.read()
@@ -17,15 +22,16 @@ def tokenize(fname):
     return taggedSents
 
 
-taggedSents1 = tokenize(
-    "/home/shirish/BTECHSEM2/project/books/stories/new_complete_stories.txt")
-taggedSents0 = tokenize(
-    "/home/shirish/BTECHSEM2/project/books/annotated_books/stories_without_anecdotes/new_all_stories.txt1")
-
-imp_cols = ["N", "VB", "VBD", "VBG", "VBN", "VBP", "VBZ"]
-
-
 def featureset_df(taggedSents, value):
+    """
+    This function returns a dataframe consisting of the imp_cols and
+    target column after removing the NaN values
+    @param taggedSents = list of sentences, where each sentence is a
+                         list of POS tagged words
+    @param value = Value given to the target column in the dataframe
+    Returns = dataframe with columns as imp_cols and target with value
+              @param value
+    """
     sents1 = []
     for i in taggedSents:
         l = {}
@@ -45,12 +51,30 @@ def featureset_df(taggedSents, value):
     return features
 
 
+taggedSents1 = tokenize(
+    "/home/shirish/BTECHSEM2/project/books/stories/new_complete_stories.txt")
+taggedSents0 = tokenize(
+    """/home/shirish/BTECHSEM2/project/books/annotated_books/
+    stories_without_anecdotes/new_all_stories.txt1""")
+
+imp_cols = ["N", "VB", "VBD", "VBG", "VBN", "VBP", "VBZ"]
+
 features1 = featureset_df(taggedSents1, 1)
-features2 = featureset_df(taggedSents1, 0)
-features = pd.concat([features1, features2])
+features0 = featureset_df(taggedSents0, 0)
+features = pd.concat([features1, features0])
 
 features = shuffle(features)
 
-model = RandomForestClassifier(n_estimators=100)
-model = model.fit(features[imp_cols].values, features["target"].values)
-model.score(features1[imp_cols].values, features1["target"].values)
+X_train, X_test, y_train, y_test = train_test_split(
+    features[imp_cols].values, features.target.values)
+
+svc = SVC(kernel='rbf')
+
+svc = svc.fit(X_train, y_train)
+
+print "Score = ", cross_val_score(svc, X_test, y_test).mean()
+
+predicted = svc.predict(X_test)
+
+print "Confusion Matrix:"
+print confusion_matrix(predicted, y_test)
